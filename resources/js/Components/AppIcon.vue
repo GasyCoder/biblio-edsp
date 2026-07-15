3926c0ec8740515478a0a75e132fb9f9a0c83e7b
GasyCoder <bezaraflorent@gmail.com>
2026-07-15 21:09:16 +0300

    refactor: compact table actions with icons

--- resources/js/Components/AppIcon.vue
+++ resources/js/Components/AppIcon.vue
@@ -22,6 +22,11 @@ defineProps<{ name: string }>();
         <template v-else-if="name === 'logout'"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9"/></template>
         <template v-else-if="name === 'user'"><circle cx="12" cy="8" r="4"/><path d="M4 21a8 8 0 0 1 16 0"/></template>
         <template v-else-if="name === 'clock'"><circle cx="12" cy="12" r="9"/><path d="M12 7v5l3 2"/></template>
+        <template v-else-if="name === 'edit'"><path d="M12 20h9"/><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L8 18l-4 1 1-4Z"/></template>
+        <template v-else-if="name === 'trash'"><path d="M3 6h18M8 6V4h8v2M19 6l-1 15H6L5 6M10 11v5M14 11v5"/></template>
+        <template v-else-if="name === 'print'"><path d="M6 9V3h12v6M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><path d="M6 14h12v7H6z"/></template>
+        <template v-else-if="name === 'check'"><path d="m5 12 4 4L19 6"/></template>
+        <template v-else-if="name === 'close'"><path d="m6 6 12 12M18 6 6 18"/></template>
         <template v-else-if="name === 'eye'"><path d="M2 12s3.5-6 10-6 10 6 10 6-3.5 6-10 6S2 12 2 12"/><circle cx="12" cy="12" r="2.5"/></template>
         <template v-else><path d="m3 3 18 18M10.6 10.6a2 2 0 0 0 2.8 2.8M9.9 4.2A10.7 10.7 0 0 1 12 4c6.5 0 10 8 10 8a18 18 0 0 1-2.1 3.1M6.6 6.6C3.6 8.6 2 12 2 12s3.5 8 10 8a10 10 0 0 0 5.4-1.6"/></template>
     </svg>
